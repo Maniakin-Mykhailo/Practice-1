3262eb896f57041bd6858e5c65a7d599023ceff8
Cremson <89212136+Maniakin-Mykhailo@users.noreply.github.com>
2021-12-09 16:30:19 +0200

    Site structure and navigation

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WebHome</title>
+</head>
+<body>
+<div class="page">
+    <header class="header">
+        <h1>
+            Hello, WebHome Education
+        </h1>
+    </header>
+    <main class="main">
+        <nav class="nav">
+            <a class="navLink" href="lesson1.php">Урок 1</a>
+            <a class="navLink" href="#">Урок 2</a>
+        </nav>
+    </main>
+
+    <footer class="footer">
+        I'm footer
+    </footer>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
---
+++ lesson1.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WebHome</title>
+</head>
+<body>
+<div class="nav">
+    <a class="navLink" href="index.php">Главная</a>
+</div>
+<main>
+    <table>
+        <tr>
+            <td>
+                <ul>
+                    <li>1 * 1 = 1</li>
+                    <li>1 * 2 = 2</li>
+                    <li>1 * 3 = 3</li>
+                    <li>1 * 4 = 4</li>
+                    <li>1 * 5 = 5</li>
+                    <li>1 * 6 = 6</li>
+                    <li>1 * 7 = 7</li>
+                    <li>1 * 8 = 8</li>
+                    <li>1 * 9 = 9</li>
+                    <li>1 * 10 = 10</li>
+
+                </ul>
+            </td>
+            <td>
+                <ul>
+                    <li>2 * 1 = 2</li>
+                    <li>2 * 2 = 4</li>
+                    <li>2 * 3 = 6</li>
+                    <li>2 * 4 = 8</li>
+                    <li>2 * 5 = 10</li>
+                    <li>2 * 6 = 12</li>
+                    <li>2 * 7 = 14</li>
+                    <li>2 * 8 = 16</li>
+                    <li>2 * 9 = 18</li>
+                    <li>2 * 10 = 20</li>
+
+                </ul>
+            </td>
+            <td>
+                <ul>
+                    <li>3 * 1 = 3</li>
+                    <li>3 * 2 = 6</li>
+                    <li>3 * 3 = 9</li>
+                    <li>3 * 4 = 12</li>
+                    <li>3 * 5 = 15</li>
+                    <li>3 * 6 = 18</li>
+                    <li>3 * 7 = 21</li>
+                    <li>3 * 8 = 24</li>
+                    <li>3 * 9 = 27</li>
+                    <li>3 * 10 = 30</li>
+                </ul>
+            </td>
+            <td>
+                <ul>
+                    <li>4 * 1 = 4</li>
+                    <li>4 * 2 = 8</li>
+                    <li>4 * 3 = 12</li>
+                    <li>4 * 4 = 16</li>
+                    <li>4 * 5 = 20</li>
+                    <li>4 * 6 = 24</li>
+                    <li>4 * 7 = 28</li>
+                    <li>4 * 8 = 32</li>
+                    <li>4 * 9 = 36</li>
+                    <li>4 * 10 = 40</li>
+                </ul>
+            </td>
+            <td>
+                <ul>
+                    <li>5 * 1 = 5</li>
+                    <li>5 * 2 = 10</li>
+                    <li>5 * 3 = 15</li>
+                    <li>5 * 4 = 20</li>
+                    <li>5 * 5 = 25</li>
+                    <li>5 * 6 = 30</li>
+                    <li>5 * 7 = 35</li>
+                    <li>5 * 8 = 40</li>
+                    <li>5 * 9 = 45</li>
+                    <li>5 * 10 = 50</li>
+                </ul>
+            </td>
+
+        </tr>
+
+    </table>
+
+    <table>
+        <tr>
+            <td>
+                <ul>
+                    <li>6 * 1 = 6</li>
+                    <li>6 * 2 = 12</li>
+                    <li>6 * 3 = 18</li>
+                    <li>6 * 4 = 24</li>
+                    <li>6 * 5 = 30</li>
+                    <li>6 * 6 = 36</li>
+                    <li>6 * 7 = 42</li>
+                    <li>6 * 8 = 48</li>
+                    <li>6 * 9 = 54</li>
+                    <li>6 * 10 = 60</li>
+                </ul>
+            </td>
+            <td>
+                <ul>
+                    <li>7 * 1 = 7</li>
+                    <li>7 * 2 = 14</li>
+                    <li>7 * 3 = 21</li>
+                    <li>7 * 4 = 28</li>
+                    <li>7 * 5 = 35</li>
+                    <li>7 * 6 = 42</li>
+                    <li>7 * 7 = 49</li>
+                    <li>7 * 8 = 56</li>
+                    <li>7 * 9 = 63</li>
+                    <li>7 * 10 = 70</li>
+                </ul>
+            </td>
+            <td>
+                <ul>
+                    <li>8 * 1 = 8</li>
+                    <li>8 * 2 = 16</li>
+                    <li>8 * 3 = 24</li>
+                    <li>8 * 4 = 32</li>
+                    <li>8 * 5 = 40</li>
+                    <li>8 * 6 = 48</li>
+                    <li>8 * 7 = 56</li>
+                    <li>8 * 8 = 64</li>
+                    <li>8 * 9 = 72</li>
+                    <li>8 * 10 = 80</li>
+                </ul>
+            </td>
+            <td>
+                <ul>
+                    <li>9 * 1 = 9</li>
+                    <li>9 * 2 = 18</li>
+                    <li>9 * 3 = 27</li>
+                    <li>9 * 4 = 36</li>
+                    <li>9 * 5 = 45</li>
+                    <li>9 * 6 = 54</li>
+                    <li>9 * 7 = 63</li>
+                    <li>9 * 8 = 72</li>
+                    <li>9 * 9 = 81</li>
+                    <li>9 * 10 = 90</li>
+                </ul>
+            </td>
+            <td>
+                <ul>
+                    <li>10 * 1 = 10</li>
+                    <li>10 * 2 = 20</li>
+                    <li>10 * 3 = 30</li>
+                    <li>10 * 4 = 40</li>
+                    <li>10 * 5 = 50</li>
+                    <li>10 * 6 = 60</li>
+                    <li>10 * 7 = 70</li>
+                    <li>10 * 8 = 80</li>
+                    <li>10 * 9 = 90</li>
+                    <li>10 * 10 = 100</li>
+                </ul>
+            </td>
+
+        </tr>
+
+    </table>
+</main>
+
+</body>
+
+</html>
